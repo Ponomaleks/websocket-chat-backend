338eed3295ff9a4ae66d2dd459513c45d309a92b
Alexander Ponomarenko <79024231+Ponomaleks@users.noreply.github.com>
2022-02-01 00:56:49 +0200

    add FE files

---
+++ README.md
@@ -0,0 +1,13 @@
+TASK
+
+1. Run the WebSocket server from task folder via command: npm run start After
+   that server will be available on ws://localhost:8080
+2. The task is to implement the chat that can broadcast messages between 2 or
+   more different windows
+3. Message should contain username, time and text content.
+
+- Pay attention that you will not receive your own messages from server, you
+  should connect at least 2 different browser windows to it, so that way one
+  will send message and the other will receive it (and vice versa).
+- You can use prompt() to ask user for his name when opening the chat.
+- You can edit any files in client folder.
---
+++ src/chat.js
@@ -0,0 +1,97 @@
+let userName;
+userName = prompt('What is yout nickname?') || 'Anonymous';
+
+const ws = new WebSocket('ws://websocket-chat-backend.glitch.me');
+const chat = document.getElementById('chat');
+const lastMsgName = document.querySelector('.active  .last-msg-name');
+const lastMsgPreview = document.querySelector(
+  '.active .chat-list-item-preview-msg',
+);
+const counter = document.querySelector('.active .new-msg-counter');
+const onlineCounter = document.querySelector('.chat-subtitle');
+
+const timeStringLength = 5;
+
+window.addEventListener('focus', () => {
+  chat.scrollTo({ top: chat.scrollHeight, behavior: 'smooth' });
+});
+
+let unreadMsgCount = 0;
+
+ws.onopen = () => {
+  ws.send(JSON.stringify({ userName }));
+};
+
+ws.onmessage = ({ data }) => {
+  const parsed = JSON.parse(data);
+
+  if (parsed.online) {
+    onlineCounter.textContent = `${parsed.online} members online`;
+  }
+
+  if (parsed.userName) {
+    chat.insertAdjacentHTML(
+      'beforeend',
+      `<p class="connection-message">${parsed.userName} has joined the chat</p>`,
+    );
+  }
+
+  if (parsed.time) {
+    const { name, message, time } = parsed;
+    chat.insertAdjacentHTML(
+      'beforeend',
+      `<p class="user-message">
+        <span class="user-name">${name}</span>
+        <span class="user-chat-message">${message}<span class="time">${time}</span></span>
+            
+      </p>
+      <div class="clear"></div>`,
+    );
+
+    lastMsgName.textContent = `${name}: `;
+    lastMsgPreview.textContent = `${message}`;
+
+    unreadMsgCount += 1;
+    unreadMsgCount !== 0
+      ? (counter.innerHTML = unreadMsgCount)
+      : (counter.innerHTML = 1);
+
+    if (document.hasFocus()) {
+      unreadMsgCount = 0;
+      counter.innerHTML = '';
+
+      chat.scrollTo({ top: chat.scrollHeight, behavior: 'smooth' });
+    }
+  }
+};
+
+const chatForm = document.getElementById('chat-form');
+chatForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const message = this.elements.message.value;
+  const time = new Date().toLocaleTimeString().slice(0, timeStringLength);
+
+  const data = {
+    name: userName,
+    message,
+    time,
+  };
+
+  ws.send(JSON.stringify(data));
+  this.reset();
+  chat.insertAdjacentHTML(
+    'beforeend',
+    `<p class="your-message">
+     <span class="your-chat-message">${message}<span class="time">${time}</span></span>
+     </p>`,
+  );
+  lastMsgName.textContent = `${userName}: `;
+  lastMsgPreview.textContent = `${message}`;
+
+  chat.scrollTo({ top: chat.scrollHeight, behavior: 'smooth' });
+  if (document.hasFocus()) {
+    unreadMsgCount = 0;
+    counter.innerHTML = '';
+    console.log('Tab is active');
+  }
+});
